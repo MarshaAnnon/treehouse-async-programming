742dd8c60257641725bbd14339d89f6904c97035
marsha <marshaannon@gmail.com>
2020-09-25 22:38:07 -0400

    complete exercise & add adjustments to styles

---
+++ 2- Asynchronous JavaScript with Callbacks/2-async-programming-and-callback-functions/start/js/promises-breakfast.js
@@ -0,0 +1,15 @@
+const order = true;
+
+const breakfastPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if(order) {
+            resolve('Your order is ready! 🥳');
+        } else {
+            reject( Error('Your order cannot be made.'))
+        }
+    }, 3000) 
+});
+
+breakfastPromise
+    .then(val => console.log(val))
+    .catch(err => console.log(err))
--- 2- Asynchronous JavaScript with Callbacks/2-async-programming-and-callback-functions/start/js/promises.js
+++ 2- Asynchronous JavaScript with Callbacks/2-async-programming-and-callback-functions/start/js/promises.js
@@ -3,36 +3,80 @@ const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
 const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
-function getJSON(url, callback) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.onload = () => {
-    if(xhr.status === 200) {
-      let data = JSON.parse(xhr.responseText);
-      return callback(data);
-    }
-  };
-  xhr.send();
+function getProfiles(json) {
+  const profiles = json.people.map(person => {
+    const craft = person.craft;
+    return fetch(wikiUrl + person.name)
+      .then(resp => resp.json())
+      .then( profile => {
+        return{...profile, craft}
+      })
+      .catch(err => console.log('Error fetching Wiki: ', err))      
+  }); 
+  return Promise.all(profiles);
 }
 
-function getProfiles(json) {
-  json.people.map( person => {
-    getJSON(wikiUrl + person.name, generateHTML);      
-  }); 
-}
+// function getJSON(url) {
+//   return new Promise( (resolve, reject) => {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', url);
+//     xhr.onload = () => {
+//       if(xhr.status === 200) {
+//         let data = JSON.parse(xhr.responseText);
+//         resolve(data);
+//       } else {
+//         reject( Error(xhr.statusText) );
+//       }
+//     };
+//     xhr.onerror = () => reject( Error('A network error occurred') );
+//     xhr.send();
+//   });
+// }
+
+// function getProfiles(json) {
+//   const profiles = json.people.map( person => {
+//     return getJSON(wikiUrl + person.name);      
+//   }); 
+//   return Promise.all(profiles);
+// }
 
 function generateHTML(data) {
-  const section = document.createElement('section');
-  peopleList.appendChild(section);
-  section.innerHTML = `
-    <img src=${data.thumbnail.source}>
-    <h2>${data.title}</h2>
-    <p>${data.description}</p>
-    <p>${data.extract}</p>
+  data.map(person => {
+    const section = document.createElement('section');
+    peopleList.appendChild(section);
+    section.innerHTML = `
+    <img src=${person.thumbnail.source}>
+    <span>${person.craft}</span>
+    <h2>${person.title}</h2>
+    <p>${person.description}</p>
+    <p>${person.extract}</p>
   `;
+  })
 }
 
 btn.addEventListener('click', (event) => {
-  getJSON(astrosUrl, getProfiles);
-  event.target.remove();
+  event.target.textContent = "loading...";
+
+  fetch(astrosUrl)
+    .then(resp => resp.json())
+    .then(getProfiles)
+    .then(generateHTML)
+    .catch( err => {
+      peopleList.innerHTML = '<h3>Something went wrong</h3>'
+      console.log(err)
+    })
+    .finally(() => event.target.remove());
 });
+
+
+// btn.addEventListener('click', (event) => {
+//   event.target.textContent = "loading...";
+//   getJSON(astrosUrl)
+//     .then(getProfiles)
+//     .then(generateHTML)
+//     .catch( err => {
+//       peopleList.innerHTML = '<h3>Something went wrong</h3>'
+//       console.log(err)
+//     })
+//     .finally( () => event.target.remove() );
+// });
